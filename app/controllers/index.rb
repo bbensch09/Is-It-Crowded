5425122aaaa1663a184f53723e1240469c005e7c
Brian Bensch <bbensch@gmail.com>
2016-01-23 16:40:29 -0800

    hovering over this commit message for too long

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -2,6 +2,13 @@ get '/' do
   erb :index
 end
 
-get '/app' do
-  erb :index
-end
+# SHOW A SPECIFIC RESTAURANT
+# get '/:id' do
+#   @restaurant = Restaurant.find(params[:id])
+#   @reviews = Review.where("restaurant_id=?",params[:id])
+#   if current_user
+#     @current_user_reviews = Review.where("restaurant_id=? AND reviewer_id=?",params[:id], current_user.id)
+#   end
+
+#   erb :show_restaurant
+# end
